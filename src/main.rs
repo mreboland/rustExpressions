2ef93efb215b5c8f1813cff4494dbc0da71eb5e5
Edward Boland <mr.eboland@gmail.com>
2021-05-20 14:24:49 -0400

    An expression language

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
     println!("Hello, world!");
+
+
+
+    // An Expression Language
+
+    // Rust resembles the C family of languages, however it's a but of a ruse. In C, there's a big distinction between expressions. Bits of code like so:
+    5 * (fahr-32) / 9
+    // and statements, like so:
+    for (; begin != end; ++begin) {
+        if (*begin == target)
+            break;
+    }
+
+    // Expressions have values, statements don't.
+
+    // Rust is what is called an expression language. This means it follows an older tradition, dating back to Lisp, where expressions do all the work.
+
+    // In C, if and switch are statements. They don't produce a value, and they can't be used in the middle of an expression. In Rust, if and match can produce values. A match expression that produces a numeric value:
+    pixels[r * bound.0 + c] =
+        match escapes(Complex { re: point.0, im: point.1}, 255) {
+            None => 0,
+            Some(count) => 255 - count as u8
+        };
+
+    // An if expression can be used to initialize a variable:
+    let status = 
+        if cpu.temperature <= MAX_TEMP {
+            HttpStatus::Ok
+        } else {
+            HttpStatus::ServerError // server melted
+        };
+
+    // A match expression can be passed as an argument to a function or macro:
+    println!("Inside the vat, you see {}.",
+        match vat.contents {
+            Some(brain) => brain.desc(),
+            None => "nothing of interest"
+        });
+
+        
 }
