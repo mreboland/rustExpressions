bcba727f58b7d2a54bcb976c65ffb4021a393699
Edward Boland <mr.eboland@gmail.com>
2021-05-20 14:35:43 -0400

    Blocks and semicolons

--- src/main.rs
+++ src/main.rs
@@ -39,5 +39,55 @@ fn main() {
             None => "nothing of interest"
         });
 
-        
+
+
+        // Blocks and Semicolons
+
+        // Blocks, too, are expressions. A block produces a value and can be used anywhere a value is needed:
+        let display_name = match post.author() {
+            Some(author) => author.name(),
+            None => {
+                let network_info = post.get_network_metadata()?;
+                let ip = network_info.client_address();
+                ip.to_string()
+            }
+        };
+
+        // The code after Some(author) => is the simple expression author.name(). The code after None => is a block expression. It makes no difference to Rust. The value of the block is the value of its last expression, ip.to_string().
+
+        // Note that there is no semicolon after that expression. Most lines of Rust code do end with either a a semicolon or curly braces. As mentioned in chapt 2, when we leave the semicolon off the last line of a block, we're making that block produce a value. The value of the final expression.
+
+        // In languages like JS, we're allowed to omit semicolons as the language auto adds them. In Rust, the semicolon actually means something.
+        let msg = {
+            // let-declaration: semicolon is always required
+            let dandelion_control = puffball.open();
+
+            // expression + semicolon: method is called, return value dropped
+            dandelion_control.release_all_seeds(launch_codes);
+
+            // expression with no semicolon: method is called, return value stored in 'msg'
+            dandelion_control.get_status()
+        };
+
+        // This ability of blocks to contain declarations and also produce a value at the end is a neat feature, one that quickly comes to feel natural. The one drawback is that it leads to an odd error message when we leave out a semicolon by accident.
+        ...
+        if preferences.changed() {
+            page.compute_size() // oops, missing semicolon
+        }
+        ...
+
+        // Rust will error out saying:
+        // mismatched types...
+        // expressions_missing_semicolon....
+
+        // Rust assumes we've omitted the semicolon on purpose. It doesn't consider the possibility that it's just a typo. A confused error message is the result. When we see expected type `()`, look for a missing semicolon first.
+
+        // Empty statements are also allowed in blocks. An empty statement consists of a stray semicolon, all by itself:
+        loop {
+            work();
+            play();
+            ; // <-- empty statement
+        }
+
+        // Rust follows the tradition of C in allowing this. Empty statements do nothing except convey a slight feeling of melancholy. We mention them only for completeness.
 }
