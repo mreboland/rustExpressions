b502c505c3f9bfda876ae8729c9ec411a8c67bef
Edward Boland <mr.eboland@gmail.com>
2021-05-20 15:13:04 -0400

    if and match

--- src/main.rs
+++ src/main.rs
@@ -147,5 +147,101 @@ fn main() {
 
 
 
+        // if and match
+
+        // The form of an if expression is familiar:
+        if condition 1 {
+            block1
+        } else if condition2 {
+            block2
+        } else {
+            block_n
+        }
+
+        // Each condition must be an expression of type bool. Rust, true to form, does not implicitly convert numbers or pointers to Boolean values. Parentheses are not required, the curly braces however, are.
+
+        // The else if blocks, as well as the final else, are optional. An if expression with no else block behaves exactly as though it had an empty else block.
+
+        // match expressions:
+        match code {
+            0 => println!("OK"),
+            1 => println!("Wires Tangles"),
+            2 => println!("User Asleep"),
+            _ => println!("Unrecognized Error {}", code)
+        }
+
+        // Exactly one of the four arms of this match expression will execute, depending on the value of code. The wildcard pattern _ matches everything, so it serves as the default case.
+
+        // The compiler can optimize this kind of match using a jump table. A similar optimization is applied when each arm of a match produces a constant value. In that case, the compiler builds an array of those values, and the match is compiled into an array access. Apart from a bounds check, there is no branching at all in the compiled code.
+
+        // The versatility of match stems from the variety of supported patterns that can be used to the left of => in each arm. Above, each pattern is simply a constant integer. We've also shown match expressions that distinguish two kinds of Option value:
+        match params.get("name") {
+            Some(name) => println!("Hello, {}", name),
+            None => println!("Greetings, stranger.")
+        }
+
+        // A pattern can match a range of value. It can unpack tuples. It can match against individual fields of structs. It can chase references, borrow parts of a value, and more. Rust's patterns are a mini-language of their own. More in chapter 10.
+
+        // The general form of a match expression is:
+        match value {
+            pattern => expr,
+            ...
+        }
+
+        // The comma after an arm may be dropped if the expr is a block.
+
+        // Rust checks the given value against each patter in turn, starting with the first. When a pattern matches, the corresponding expr is evaluated and the match expression is complete. No further patterns are checked. At least one of the patterns must match. Rust prohibits match expressions that do not cover all possible values:
+        let score = match card.rank {
+            Jack => 10,
+            Queen => 10,
+            Ace => 11
+        }; // error, non-exhaustive patterns
+
+        // All blocks of an if expression must produce values of the same type:
+        let suggested_pet =
+            if with_wings { Pet::Buzzard } else { Pet::Hyena }; // ok
+
+        let favourite_number =
+            if user.is_hobbit() { "eleventy-one" } else { 9 }; // error
+
+        let best_sports_team =
+            if is_hockey_season() { "Predators" }; //error
+
+        // The last example is an error because in July, the result would be ().
+
+        // Similarly, all arms of a match expression must have the same type:
+        let suggested_pet =
+            match favourites.element {
+                Fire => Pet::RedPanda,
+                Air => Pet::Buffalo,
+                Water => Pet::Orca,
+                _ => None // error, incompatible types
+            };
+
+        // if let
+        if let patter = expr {
+            block1
+        } else {
+            block2
+        }
+
+        // The given expr either matches the pattern, in which case block 1 runs, or it doesn't, and block2 runs. Sometimes this is a nice way to get data out of an Option or Result:
+        if let Some(cookie) = request.session_cookie {
+            return restore_session(cookie);
+        }
+
+        if let Err(err) = present_cheesy_anti_robot_task() {
+            log_robot_attempt(err);
+            politely_accuse_user_of_being_a_robot();
+        } else {
+            session.mark_as_human();
+        }
+
+        // It's never strictly necessary to use if let, because match can do everything if let can do. An if let expression is shorthand for a match with just one patter:
+        match expr {
+            patter => { block1 }
+            _ => { block2 }
+        }
+
         
 }
